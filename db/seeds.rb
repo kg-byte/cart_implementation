d92fba921f874472d41a1dfe7d628e20e328fd15
Brian Zanti <brianzanti@gmail.com>
2020-10-27 09:40:18 -0600

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
+Artist.destroy_all
+
+talking_heads = Artist.create!(name: "Talking Heads")
+journey = Artist.create!(name: "Journey")
+zac_brown = Artist.create!(name: "Zach Brown Band")
+
+talking_heads.songs.create!(title: "This Must be the Place", length: 345, play_count: 23)
+talking_heads.songs.create!(title: "Heaven", length: 432, play_count: 12)
+journey.songs.create!(title: "Don't Stop Believin'", length: 367, play_count: 45)
+zac_brown.songs.create!(title: "Chicken Fried", length: 183, play_count: 49)
