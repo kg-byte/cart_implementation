889a966e817a9413d429095f730b5f2a9080eaf0
Kim G <xiaole.guo@yahoo.com>
2022-05-02 21:02:22 -0600

    complete authorization tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-
+  enum role: %w(default admin)
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  namespace :admin do 
+    get 'dashboard', to: 'dashboard#index'
+  end
+
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
   get '/profile', to: 'users#show'
